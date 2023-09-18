ad9a533978b0f089b1dfa0310c8c88cee3064854
Ricardo-Roch <ricky.jr.rocha@hotmail.com>
2023-09-18 17:25:02 -0600

    Realizacion de la practica 2, Normalizamos el csv y gurdamos unas columnas en un par de csv, y agregamos el diagrama

--- P2_main.py
+++ P2_main.py
@@ -6,12 +6,13 @@ df = pd.read_csv('Nacimientos2020-2023.csv')
 #Eliminar columas inecesarias
 df = df.drop('anio', axis=1)
 df = df.drop('mes', axis=1)
-df = df.drop('tutora_1_edad', axis=1)
-df = df.drop('tutora_1_sexo', axis=1)
-df = df.drop('tutora_1_nacionalidad', axis=1)
-df = df.drop('tutora_2_edad', axis=1)
-df = df.drop('tutora_2_sexo', axis=1)
-df = df.drop('tutora_2_nacionalidad', axis=1)
+#df = df.drop('tutora_1_edad', axis=1)
+#df = df.drop('tutora_1_sexo', axis=1)
+#df = df.drop('tutora_1_nacionalidad', axis=1)
+#df = df.drop('tutora_2_edad', axis=1)
+#df = df.drop('tutora_2_sexo', axis=1)
+#df = df.drop('tutora_2_nacionalidad', axis=1)
+
 #Realizo cambios en los nombres de los registros
 #Cambios
 reemplazos = {
@@ -23,8 +24,6 @@ reemplazos = {
 df['estado_nacimiento'] = df['estado_nacimiento'].replace(reemplazos)
 #Guarda el archivo ya normalizado
 df.to_csv('Actualizacion-Nacimientos2020-2023.csv', index=False)
-
-
 
 
 
